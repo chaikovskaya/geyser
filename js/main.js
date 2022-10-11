a4be301b8770d8747cbeb8c9d24b5d0b8dad8575
elena <post-123@yandex.ru>
2022-10-11 09:24:07 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,285 @@
+/*--GLOBAL--*/
+var GLOBAL = GLOBAL || {};
+GLOBAL.widthWindow = GLOBAL.widthWindow || {};
+GLOBAL.FORMERROR = GLOBAL.FORMERROR || {};
+GLOBAL.FORMERROR.REQUIRED = GLOBAL.FORMERROR.REQUIRED || '';
+GLOBAL.FORMERROR.EMAIL = GLOBAL.FORMERROR.EMAIL || '';
+GLOBAL.mobile = GLOBAL.mobile || 768;
+GLOBAL.tablet = GLOBAL.tablet || 992;
+GLOBAL.columnsStartLength = GLOBAL.columnsStartLength || 0;
+
+GLOBAL.parseData = function parseData(data) {
+    try {
+        data = JSON.parse(data.replace(/'/gim, '"'));
+    } catch(e) {
+        data = {};
+    }
+    return data;
+};
+
+
+GLOBAL.owl = GLOBAL.owl || {};
+GLOBAL.owl.common = GLOBAL.owl.common || {};
+GLOBAL.owl.common.loop = true;
+GLOBAL.owl.common.dots = false;
+GLOBAL.owl.common.margin = 0;
+GLOBAL.owl.common.responsiveClass = true;
+GLOBAL.owl.common.autoHeight = true;
+GLOBAL.owl.common.mouseDrag = true;
+GLOBAL.owl.common.nav = false;
+/*--/global--*/
+
+function isMobile() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function initDropdown() {
+    if (typeof(Dropdown) === 'undefined' || !jQuery.isFunction(Dropdown)) {
+        return false;
+    }
+
+    var common = {};
+
+    $('.JS-Dropdown').each(function(){
+        var local = GLOBAL.parseData(jQuery(this).data('dropdown'));
+        new Dropdown(this, jQuery.extend({}, common, local));
+    });
+}
+
+function initScroll() {
+    $('.js-custom-scroll').each(function(){
+        var customScroll = this;
+        new SimpleBar(customScroll, {
+            autoHide: false
+        });
+    });
+}
+
+function initValidate($element) {
+    if (typeof($element) == 'undefined') {
+        $element = $('.js-form-validate');
+    }
+
+    $element.each(function() {
+        var $element = jQuery(this),
+            validator;
+
+        validator = $element.validate({
+            errorClass: 'form-error',
+            validClass: 'form-success',
+            submitHandler: function(form) {
+                if (typeof(ajaxSubmit) == 'function') {
+                    ajaxSubmit(form);
+                }
+            },
+        });
+
+        $.validator.messages.required = GLOBAL.FORMERROR.REQUIRED;
+        $.validator.messages.email = GLOBAL.FORMERROR.EMAIL;
+    });
+}
+
+function initMask() {
+    $('.js-mask-phone').inputmask({
+        mask: '+7 999 999 99 99',
+        "tabThrough": true,
+        "showMaskOnHover": false,
+    });
+
+    $('.js-mask-email').inputmask({
+        alias: "email",
+        "tabThrough": true,
+        "showMaskOnHover": false,
+    });
+}
+
+function initPopup() {
+    $(".js-popup").fancybox({
+        toolbar  : false,
+        smallBtn : true,
+        btnTpl: {
+            smallBtn:
+                '<button type="button" data-fancybox-close class="fancybox-close" title="{{CLOSE}}">' +
+                '<i class="fancybox-close-icon"></i>' +
+                '</button>'
+        },
+        lang: "ru",
+        i18n: {
+            ru: {
+                CLOSE: "Закрыть",
+            },
+        },
+    });
+}
+
+function initSelect() {
+    $('.js-select').selectric({
+        disableOnMobile: false,
+        nativeOnMobile: false,
+        arrowButtonMarkup: '<b class="selectric-button"><i class="selectric-icon"></i></b>',
+    });
+}
+
+var sliderMainBanner;
+function initSliderMainBanner() {
+    jQuery('.js-slider-main-banner').each(function() {
+        var $slider = $(this),
+            sliderLength = $slider.find('.swiper-slide').length,
+            $count = $slider.find('.js-slider-count');
+
+        var isStart = sliderLength > 1 ? true : false;
+
+        sliderMainBanner = new Swiper($slider[0], {
+            loop: isStart,
+            pagination: false,
+            navigation: {
+                nextEl: $slider.find('.js-slider-next')[0],
+                prevEl: $slider.find('.js-slider-prev')[0],
+                disabledClass: "slider-button_disabled",
+            },
+            slidesPerView: 'auto',
+            threshold: 10,
+            breakpoints: {
+                0: {
+                    simulateTouch: false,
+                    spaceBetween: 15,
+                },
+                768: {
+                    spaceBetween: 16,
+                },
+                992: {
+                    spaceBetween: 28,
+                },
+            },
+            on: {
+                beforeInit: function () {
+                },
+                init: function () {
+                },
+                slideChangeTransitionEnd: function () {
+                    var index = $slider.find('.swiper-slide-active').data('slider-index');
+                    if (index < 10) {
+                        index = '0' + index;
+                    }
+                    $count.text(index);
+                },
+            },
+        });
+    });
+}
+
+function initMobileMenu() {
+    if (typeof(MobileMenu) === 'undefined' || !jQuery.isFunction(MobileMenu)) {
+        return false;
+    }
+
+    var common = {};
+
+    jQuery('.JS-MobileMenu').not('.JS-MobileMenu-ready').each(function() {
+        var local = GLOBAL.parseData(jQuery(this).data('mobilemenu'));
+        new MobileMenu(this, jQuery.extend({}, common, local));
+    });
+}
+
+function initForm() {
+    jQuery('.js-form').each(function() {
+        var $checkbox = $(this).find('.js-form-checkbox'),
+            $button = $(this).find('.js-form-button'),
+            classDisabled = $(this).data('form-disabled');
+
+        if ($checkbox.is(':checked')) {
+            $button.removeClass(classDisabled);
+        } else {
+            $button.addClass(classDisabled);
+        }
+
+        $checkbox.on("change", function(e) {
+            e.stopPropagation();
+            if ($checkbox.is(':checked')) {
+                $button.prop("disabled", false);
+                $button.removeClass(classDisabled);
+            } else {
+                $button.prop("disabled", true);
+                $button.addClass(classDisabled);
+            }
+        });
+    });
+}
+
+function openPopupSuccess(url) {
+    if (typeof(url) == 'undefined') {
+        url = '/';
+    }
+
+    $.fancybox.open({
+        src  : url,
+        type : 'ajax',
+        toolbar  : false,
+        smallBtn : true,
+        afterShow: function (data) {
+        },
+        btnTpl: {
+            smallBtn:
+                '<button type="button" data-fancybox-close class="fancybox-close" title="{{CLOSE}}">' +
+                '<i class="fancybox-close-icon"></i>' +
+                "</button>"
+        },
+        lang: "ru",
+        i18n: {
+            ru: {
+                CLOSE: "Закрыть",
+            },
+        }
+    });
+}
+
+function initAjaxMore() {
+    if (typeof(AjaxMore) === 'undefined' || !jQuery.isFunction(AjaxMore)) {
+        return false;
+    }
+
+    var common = {
+        beforeSend: function () {
+        },
+        success: function () {
+        }
+    };
+
+    $('.JS-AjaxMore').each(function(){
+        var local = GLOBAL.parseData(jQuery(this).data('ajaxmore'));
+        new AjaxMore(this, jQuery.extend({}, common, local));
+    });
+}
+
+function initResizeWindow() {
+    var width = $(window).outerWidth();
+    if (width <= GLOBAL.mobile) {
+        GLOBAL.widthWindow = 'isMobile';
+    } else if (width <= GLOBAL.tablet) {
+        GLOBAL.widthWindow = 'isTablet';
+    } else {
+        GLOBAL.widthWindow = '';
+    }
+}
+
+$(document).ready(function () {
+    initResizeWindow();
+    $(window).resize(function(){
+        initResizeWindow();
+    });
+
+    initDropdown();
+    initScroll();
+    initValidate();
+    initMask();
+    initPopup();
+    initSelect();
+    initSliderMainBanner();
+    initMobileMenu();
+    initForm();
+    initAjaxMore();
+});
